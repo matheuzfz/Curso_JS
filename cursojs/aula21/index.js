ea47cfc071698289fb8c52fcf490aa2c3c7b00b7
Matheus F. dos Santos <77494755+matheuzfz@users.noreply.github.com>
2023-06-15 21:41:39 -0300

    Add files via upload

---
+++ cursojs/aula21/index.js
@@ -0,0 +1,13 @@
+/*
+Operadores de comparação
+> Maior que
+>= Maior ou igual a
+< Menor que
+<= Menor que ou igual a
+== Igualdade (Valor)
+=== Igualdade estrita (Valor e Tipo)
+!= Diferente (Valor)
+!== Diferente estrito (Valor e Tipo)
+*/
+
+console.log(10>5)
---
+++ cursojs/aula3/index.js
@@ -0,0 +1,5 @@
+console.log('Hello World'); // String = Texto
+console.log("Hello World"); // Number = Número
+console.log(`Hello World`);
+console.log(`'Hello' "World"`);
+console.log(123456, 15.85, 'Hello World Again');
